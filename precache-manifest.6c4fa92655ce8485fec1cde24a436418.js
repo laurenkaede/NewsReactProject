01aa3f355073768e8ef35115b6f6e4c2c8f313e2
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 15:58:02 +0100

    Updates

---
+++ precache-manifest.6c4fa92655ce8485fec1cde24a436418.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c15990e2c778416a8163108bae9a56b",
+    "url": "/NewsReactProject/index.html"
+  },
+  {
+    "revision": "45de534aa71086bca998",
+    "url": "/NewsReactProject/static/css/main.b05e24d7.chunk.css"
+  },
+  {
+    "revision": "eb1343cd5f379936a839",
+    "url": "/NewsReactProject/static/js/2.ab5ff375.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NewsReactProject/static/js/2.ab5ff375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45de534aa71086bca998",
+    "url": "/NewsReactProject/static/js/main.7c60b2de.chunk.js"
+  },
+  {
+    "revision": "523d56f1c24c1a0cf790",
+    "url": "/NewsReactProject/static/js/runtime-main.ddd292a6.js"
+  },
+  {
+    "revision": "f5a04e71ffdf68de81b5ef936aaa9fa0",
+    "url": "/NewsReactProject/static/media/newsImg1.f5a04e71.png"
+  },
+  {
+    "revision": "602b2b8c63df4ae0e45e84d11539cd9b",
+    "url": "/NewsReactProject/static/media/newsImg2.602b2b8c.png"
+  },
+  {
+    "revision": "9229b242406588bf8a8842479d332c55",
+    "url": "/NewsReactProject/static/media/newsImg3.9229b242.png"
+  },
+  {
+    "revision": "dfba68318f805b5a1fa5abf50c6f019d",
+    "url": "/NewsReactProject/static/media/newsImg4.dfba6831.png"
+  },
+  {
+    "revision": "bd9a580a86f5e2dd225018b5d74eec1b",
+    "url": "/NewsReactProject/static/media/newsImg5.bd9a580a.png"
+  },
+  {
+    "revision": "aea6a6271dddc4d1fdaf645a9c4593e0",
+    "url": "/NewsReactProject/static/media/newsImg6.aea6a627.png"
+  }
+]);
